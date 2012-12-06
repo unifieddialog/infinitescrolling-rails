e16f46062de86311000565b3bfc869a4d736c0dc
Leif Bladt <leif.bladt@gmx.de>
2012-12-06 16:39:45 +0100

    Enable more than one instance of this plugin at the same time

--- vendor/assets/javascripts/infinitescrolling.js
+++ vendor/assets/javascripts/infinitescrolling.js
@@ -1,37 +1,51 @@
 (function($){
-  var my = {
-    marginThreshold: 20,
+  function IS(loadCallback, target) {
+    var my = {
+      loadCallback: loadCallback,
+      target : target,
+      marginThreshold: 20,
+      delay: 250,
+      hasScrolled: false,
 
-    delay: 250,
+      callback: function(){
+        if (!my.hasScrolled){
+          my.hasScrolled = true;
+          setTimeout(my.checkScroll, my.delay);
+        }
+      },
 
-    hasScrolled: false,
-  
-    callback: function(){
-      if (!my.hasScrolled){
-        my.hasScrolled = true;
-        setTimeout(my.checkScroll, my.delay);
+      checkScroll: function() {
+        my.hasScrolled = false;
+        if (my.lowEnough()) {
+          my.loadCallback();
+        }
+      },
+
+      lowEnough: function() {
+        if (this.target[0] === window || this.target[0] === document) {
+          var pageHeight = $(document).height();
+          var viewportHeight = $(window).height();
+          var scrollHeight = $(document).scrollTop();
+          return pageHeight - viewportHeight - scrollHeight < my.marginThreshold;
+        } else {
+          var pageHeight = $(this.target).find('[data-infinite-scrolling]').height();
+          var viewportHeight = $(this.target).height();
+          var scrollHeight = $(this.target).scrollTop();
+          return pageHeight - viewportHeight - scrollHeight < my.marginThreshold;
+        }
       }
-    },
+    }
 
-    checkScroll: function() {
-      my.hasScrolled = false;
-      if (my.lowEnough()) {
-        my.loadCallback();
-      }
-    },
-
-    lowEnough: function() {
-      var pageHeight = $(document).height();
-      var viewportHeight = $(window).height();
-      var scrollHeight = $(document).scrollTop();
-      return pageHeight - viewportHeight - scrollHeight < my.marginThreshold;
+    IS.prototype.callback = function() {
+      my.callback();
     }
   };
 
   $.fn.infiniteScrolling = function(loadCallback){
-    my.loadCallback = loadCallback;
-    this.bind('scroll', my.callback);
-    this.bind('resize', my.callback);
+    var is = new IS(loadCallback, this);
+
+    this.bind('scroll', is.callback);
+    this.bind('resize', is.callback);
     return this;
   };
 })(jQuery);
